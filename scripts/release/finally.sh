e1d32c6b2afec65bffbc600b837e0b1b03e25dfc
PJCampi <pierrejean.campigotto@gmail.com>
2020-12-27 23:11:49 +0100

    finally uses version reset instead of git reset again

--- scripts/release/finally.sh
+++ scripts/release/finally.sh
@@ -2,5 +2,5 @@
 echo cleaning up build directories
 "$(dirname "$0")"/clean.sh
 
-echo reset all changes made to the repository
-git reset --hard HEAD
+echo reset version to 0.0.0
+"$(dirname "$0")"/add_version.sh "0.0.0"
