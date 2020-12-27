97c5db77cd9383a391ef6fbfbe161864eadf284e
PJCampi <pierrejean.campigotto@gmail.com>
2020-12-27 23:06:06 +0100

    make sure we don't attempt to publish local releases: we want to build anyways.

--- scripts/release/publish.sh
+++ scripts/release/publish.sh
@@ -13,23 +13,24 @@ CURRENT_BRANCH=$(git rev-parse --abbrev-ref --symbolic-full-name HEAD)
 # build and publish if the commit was tagged with a version
 if [[ "$CURRENT_TAG" != "" ]]
 then
-  CURRENT_VERSION=${CURRENT_TAG:1}
-  if [[ "$CURRENT_TAG" =~ "+" ]] && [[ "$DISALLOW_LOCAL_VERSIONS" == "true" ]]
-  then
-    echo "version: $CURRENT_VERSION cannot be uploaded to $REPOSITORY because local versions f.ex. alpha and post releases are not allowed."
-  else
+    CURRENT_VERSION=${CURRENT_TAG:1}
     echo adding version: $CURRENT_VERSION to package
     "$(dirname "$0")"/add_version.sh $CURRENT_VERSION
 
     echo build package
     poetry build --format wheel
 
+  if [[ "$CURRENT_TAG" =~ "+" ]] && [[ "$DISALLOW_LOCAL_VERSIONS" == "true" ]]
+  then
+    echo "version: $CURRENT_VERSION cannot be uploaded to $REPOSITORY because local versions f.ex. alpha and post releases are not allowed."
+  else
     echo publish package "$(if [[ "$PUBLISH" != "true" ]]; then echo "in dry-run mode"; fi)"
     PUBLISH_ARGS=" --username $REPOSITORY_USERNAME --password $REPOSITORY_PASSWORD"
     if [[ "$REPOSITORY" != "" ]]; then PUBLISH_ARGS+=" --repository $REPOSITORY"; fi
     if [[ "$PUBLISH" != "true" ]]; then PUBLISH_ARGS+=" --dry-run"; fi
     poetry publish $PUBLISH_ARGS
   fi
+
 else
   echo the package will not be published because no version was provided.
 fi
